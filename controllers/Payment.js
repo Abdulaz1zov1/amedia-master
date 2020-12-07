1682362d25ef79b5fdc14b42e54b1a55a2cddb2c
Umidbek Jumaniyozov <jumaniyozovu@gmail.com>
2020-12-07 14:15:17 +0500

    Payment

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -1,30 +1,30 @@
-const _ = require('lodash')
-const request = require('request');
-const { initializePayment } = require('../payment/Click')(request);
-
-exports.createClickRequest = (req,res) => {
-    const form = _.pick(
-        req.body,
-        [
-            'service_id',
-            'amount',
-            'phone_number',
-            'merchant_trans_id',
-            'full_name'
-        ]);
-    form.metadata = {
-        full_name: form.full_name
-    }
-    form.amount *= 100;
-
-    initializePayment(form, (error, body) => {
-        if (error) {
-            //handle errors
-            console.log(error);
-            return res.send(error);
-        }
-        let response = JSON.parse(body);
-        console.log(response);
-        res.send(response)
-    });
-}
+// const _ = require('lodash')
+// const request = require('request');
+// const { initializePayment } = require('../payment/Click')(request);
+//
+// exports.createClickRequest = (req,res) => {
+//     const form = _.pick(
+//         req.body,
+//         [
+//             'service_id',
+//             'amount',
+//             'phone_number',
+//             'merchant_trans_id',
+//             'full_name'
+//         ]);
+//     form.metadata = {
+//         full_name: form.full_name
+//     }
+//     form.amount *= 100;
+//
+//     initializePayment(form, (error, body) => {
+//         if (error) {
+//             //handle errors
+//             console.log(error);
+//             return res.send(error);
+//         }
+//         let response = JSON.parse(body);
+//         console.log(response);
+//         res.send(response)
+//     });
+// }
