abd9fb510514b79f15ab12ef5556ee0d763a2221
Shahriyor <shahriyornodejs@mail.ru>
2020-12-17 14:30:53 +0500

    dfdklfjdskjkfljdsf

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -2,6 +2,8 @@ const Slider = require('../models/slider')
 const asynHandler = require('../middlewares/async')
 const News = require('../models/news')
 const Anotatsiya = require('../models/anotatsiya')
+const Kino = require('../models/kino');
+const Season = require('../models/season')
 
 exports.index = asynHandler(async (req,res,next)=>{
     const sliders = await Slider.find()
@@ -19,10 +21,27 @@ exports.index = asynHandler(async (req,res,next)=>{
     const anotatsiya = await Anotatsiya.find({status: true})
         .sort({date: -1})
         .limit(1)
+    const kino = await Kino.find()
+        .skip((pageNumber - 1 )* 20)
+        .limit(20)
+        .sort({date: -1})
+        .select({name: 1, category: 1,type: 1, image: 1, rating: 1})
+        .populate({path: 'category', select: 'nameuz'})
+
+
+    const season = await Season.find()
+        .skip((pageNumber - 1 )* 20)
+        .limit(20)
+        .sort({date: -1})
+        .select({name: 1, category: 1, image: 1, rating: 1})
+        .populate({path: 'category', select: 'nameuz'})
+
     res.status(200).json({
         success: true,
         sliders,
         news,
-        anotatsiya
+        anotatsiya,
+        kino,
+        season
     })
 })
