93c913d6b2f74255111322342cdf275f2a03cac1
Shahriyor <shahriyornodejs@mail.ru>
2020-12-18 12:03:01 +0500

    dsffgdsaf

--- models/user.js
+++ models/user.js
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema({
     role : {
         type : String,
         enum : ['user', 'publisher','admin'],
-        default: 'user'
+        default: 'admin'
     },
     status : {
         type: String,
